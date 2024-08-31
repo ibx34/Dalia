ea7f45e22fdff70960f7ad5abea48b4ddf759ea0
ibx34 <guamjust@gmail.com>
2024-08-31 14:18:39 -0400

    lexer lexes

--- boot/Main.hs
+++ boot/Main.hs
@@ -1,12 +1,15 @@
 import Control.Monad.State
+import qualified Data.Map as Map
+import Data.Char (isAlpha, isAlphaNum)
 
-data TokTy = OpenP | CloseP | DColon | Colon | Eq | Dash | GThan | LThan | OpenCurlP | CloseCurlP | OpenSquareP | CloseSquareP
+-- StrLit is temporary
+data TokTy = Identifier String | StrLit String | Bang | Colon | OpenP | CloseP | OpenCurlP | CloseCurlP | Eq
+    deriving (Show, Eq)
 
 data Context a = Context
-  { input   :: a,
-    at      :: Int,
-    current :: Char,
-    results :: [Char]
+  { input :: a,
+    at :: Int,
+    results :: [TokTy]
   }
   deriving (Show)
 
@@ -17,21 +20,63 @@ initialLexer input =
   Context
     { input = input,
       at = 0,
-      current = head input,
       results = []
     }
 
-lexAll :: Lexer [Char]
+lexOne :: Char -> Maybe TokTy
+lexOne x = case x of
+    '!' -> Just Bang
+    ':' -> Just Colon
+    '(' -> Just OpenP
+    ')' -> Just CloseP
+    '{' -> Just OpenCurlP
+    '}' -> Just CloseCurlP
+    '=' -> Just Eq
+    _   -> Nothing
+
+lexIdentifier :: String -> (TokTy, Int)
+lexIdentifier cs =
+    let (ident, rest) = span isIdentChar cs
+    in (Identifier ident, length ident)
+
+lexStrLit :: String -> (TokTy, Int)
+lexStrLit cs =
+    let (ident, rest) = span isStrChar cs
+    in (StrLit ident, length ident)
+
+isStrChar :: Char -> Bool
+isStrChar c = c /= '"'
+
+isIdentChar :: Char -> Bool
+isIdentChar c = isAlphaNum c || c == '_'
+
+lexAll :: Lexer [TokTy]
 lexAll = do
   ctx <- get
   let i = at ctx
   if i >= length (input ctx)
     then return (reverse $ results ctx)
     else do
-      let newChar = input ctx !! i
-      put ctx { at = i + 1, current = newChar, results = newChar : results ctx }
-      lexAll
+      let currentChar = input ctx !! i
+      if isAlphaNum currentChar || currentChar == '_'
+        then do
+          let (identifierToken, len) = lexIdentifier (drop i (input ctx))
+          put ctx {at = i + len, results = identifierToken : results ctx}
+          lexAll
+        else if currentChar == '"'
+         then do
+           let (stringLiteralToken, len) = lexStrLit (drop (i + 1) (input ctx))
+           put ctx {at = i + len + 2, results = stringLiteralToken : results ctx}
+           lexAll
+        else case lexOne currentChar of
+          Just token -> do
+            put ctx {at = i + 1, results = token : results ctx}
+            lexAll
+          Nothing -> do
+            put ctx {at = i + 1}  -- Skip unknown characters
+            lexAll
 
+-- Example usage with a file read and lexing
 main :: IO ()
 main = do
   readdFile <- readFile "./tests/1.cyl"
