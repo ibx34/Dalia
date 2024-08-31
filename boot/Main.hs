f2d4c03eeaa80806570d2c0bda33e8fab84afbbb
ibx34 <guamjust@gmail.com>
2024-08-30 23:35:02 -0400

    this is gettign started

--- boot/Main.hs
+++ boot/Main.hs
@@ -1,24 +1,40 @@
-module Main where
-
-import GHC.IO.FD (openFile)
-import GHC.IO.IOMode (IOMode (ReadMode))
 import Control.Monad.State
 
+data TokTy = OpenP | CloseP | DColon | Colon | Eq | Dash | GThan | LThan | OpenCurlP | CloseCurlP | OpenSquareP | CloseSquareP
+
 data Context a = Context
-  { input :: a,
-    at :: Int
+  { input   :: a,
+    at      :: Int,
+    current :: Char,
+    results :: [Char]
   }
   deriving (Show)
 
-type Lexer a =  State (Context String)
+type Lexer a = State (Context String) a
 
 initialLexer :: String -> Context String
-initialLexer input = Context {
-    input = input,
-    at = 0
-}
+initialLexer input =
+  Context
+    { input = input,
+      at = 0,
+      current = head input,
+      results = []
+    }
+
+lexAll :: Lexer [Char]
+lexAll = do
+  ctx <- get
+  let i = at ctx
+  if i >= length (input ctx)
+    then return (reverse $ results ctx)
+    else do
+      let newChar = input ctx !! i
+      put ctx { at = i + 1, current = newChar, results = newChar : results ctx }
+      lexAll
 
 main :: IO ()
 main = do
-  testFile <- readFile "./tests/1.cyl"
-  print $ initialLexer testFile
+  readdFile <- readFile "./tests/1.cyl"
+  let lexer = initialLexer readdFile
+      result = evalState lexAll lexer
+  print result
