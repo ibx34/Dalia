344a9895d400fcfcb4319643f4ae3f3ee698dc4f
ibx34 <guamjust@gmail.com>
2024-10-05 22:16:10 -0400

    whatever

--- boot/Parser.hs
+++ boot/Parser.hs
@@ -5,7 +5,7 @@
 
 module Parser where
 
-import Common (Context (Context, at, c_multi_item, input, is_comment, results, sym_tables, using), Expr (Place, Reference, id, symbol_table), Keywords (..), LexerToken (..), Literals (..), Primes (Type), SymbolInfo (SymbolInfo, name, val), SymbolTable (SymbolTable, name_to_id, next_id, parent, table), SymbolType (PrimitiveType), isCurrentMultiItemComment, isWorkingOnMultiItem)
+import Common (Context (Context, at, c_multi_item, input, is_comment, results, sym_tables, using), Expr (Lambda, Place, Reference, expr, id, symbol_table, symbols), Keywords (..), LexerToken (..), Literals (..), Primes (Type), SymbolInfo (SymbolInfo, name, val), SymbolTable (SymbolTable, name_to_id, next_id, parent, table), SymbolType (PrimitiveType), isCurrentMultiItemComment, isWorkingOnMultiItem)
 import Control.Monad (void, when)
 import Control.Monad qualified
 import Control.Monad.State (MonadState (get, put), State, gets, join, modify)
@@ -127,7 +127,10 @@ insertSymbol st n e = do
 -- optioanl value should be handlered her
 parseArgList :: SymbolTable -> Maybe LexerToken -> Parser SymbolTable
 parseArgList a (Just DColon) = do
-  modify (\ctx -> ctx {at = at ctx + 1})
+  modify (\ctx -> ctx {at = at ctx + 2})
+  current >>= \case
+    Just FunctionArrow ->
+      modify (\ctx -> ctx {at = at ctx + 1})
   return a
 parseArgList a (Just OpenP) = do
   modify (\ctx -> ctx {at = at ctx + 1})
@@ -157,15 +160,27 @@ parse (Literal (Ident i)) = do
   current >>= \case
     Just Backslash -> do
       modify (\ctx -> ctx {at = at ctx + 1})
-      current <- current
+      current' <- current
       -- I call this starting_symbol_table instead of argument list
       -- smth like that because I have decided that there will be
       -- no clear disctinction of lambda symbol table and the lambda
       -- argument list because at the end of the day the arguments
       -- introducted will have to be in the symbol table to be used.
-      starting_symbol_table <- parseArgList SymbolTable {table = Map.empty, name_to_id = Map.empty, parent = Nothing, next_id = 0} current
-      error ("Lambda starting symbol table:\n\n" ++ show starting_symbol_table)
-      return (Just Place)
+      -- error ("Lambda starting symbol table:\n\n" ++ show starting_symbol_table)
+      symbols <- parseArgList SymbolTable {table = Map.empty, name_to_id = Map.empty, parent = Nothing, next_id = 0} current'
+      ctx <- get
+      modify (\ctx -> ctx {at = at ctx + 1})
+      current >>= \case
+        Just c -> do
+          parsed <- parse c
+          return
+            ( Just
+                Lambda
+                  { symbols = symbols,
+                    expr = fromMaybe Place parsed
+                  }
+            )
+        _ -> error "WHATEVER"
     Just (Literal (Ident i)) -> do
       modify (\ctx -> ctx {at = at ctx + 1})
       (symbol, id, symbol_table) <- getSymbol i Nothing
