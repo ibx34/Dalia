8229f955f48368cbac9f14d70113b9cf8c4656be
ibx34 <guamjust@gmail.com>
2024-11-21 20:28:50 -0500

    MORE MORE MORE

--- boot.py
+++ boot.py
@@ -11,9 +11,29 @@ class TT(Enum):
     BACKSLASH = "\\"
     FUNCTION_ARROW = "→"
     PLUS = "+"
+    # minus!
+    DASH = "-"
     DOUBLE_COLON = "::"
+    OPEN_PAREN = "("
+    CLOSE_PAREN = ")"
+    OPEN_SQUARE = "["
+    CLOSE_SQUARE = "]"
     IDENT = "IDENT"
     STRING = "STRING"
+
+
+operators = {
+    "+": {
+        "precedence": 1,
+        # 0 = Left, 1 = Right, 2 = None
+        "associativity": 0,
+    },
+    "-": {
+        "precedence": 1,
+        # 0 = Left, 1 = Right, 2 = None
+        "associativity": 0,
+    },
+}
 
 
 class Token:
@@ -56,7 +76,7 @@ class Lexer(Cursor):
 
     def lex_all(self) -> None:
         while c := self.current():
-            if c == " ":
+            if c == " " or c == "\n":
                 self.advance()
                 continue
             elif c == None:
@@ -88,20 +108,46 @@ class Lexer(Cursor):
 
 
 class Expr(ABC):
+    # This is a TEMPORARY SOLUTION!
+    def __init__(self, num_of_advances: int) -> None:
+        super().__init__()
+        self.num_of_advances = num_of_advances
+
     @abstractmethod
     def __repr__(self) -> str:
         pass
 
 
+def get_op(possible_op: Token | None) -> tuple[str, dict[str, int]] | None:
+    if (
+        possible_op is None
+        or possible_op.ty is None
+        or possible_op.ty not in [TT.PLUS, TT.DASH]
+    ):
+        return None
+    op = operators[possible_op.ty.value]
+    return (possible_op.ty.value, op)
+
+
+def check_is_allowed(expr: Expr | None) -> bool:
+    allowed = expr is not None or (
+        isinstance(expr, Parenthesized) or isinstance(expr, Reference)
+    )
+    if expr is not None and isinstance(expr, Identifier) and expr.for_assignment:
+        allowed = False
+    return allowed
+
+
 class Symbol:
-    def __init__(self, name: str, val: Expr, belongs_to: int) -> None:
+    def __init__(self, name: str, val: Expr, belongs_to: int, id: int) -> None:
         super().__init__()
         self.name = name
         self.val = val
         self.belongs_to = belongs_to
+        self.id = id
 
     def __repr__(self) -> str:
-        return f'Symbol "{self.name}", value = {self.val}. Belongs to s.t. {self.belongs_to}'
+        return f'Symbol "{self.name}", value = {self.val}. Belongs to = {self.belongs_to}. ID = {self.id}'
 
 
 class SymbolTable:
@@ -124,8 +170,8 @@ class SymbolTable:
         return self.symbols[id]
 
     def insert(self, name: str, val: Expr) -> None:
-        symbol = Symbol(name, val, self.id)
         self.last_id += 1
+        symbol = Symbol(name, val, self.id, self.last_id)
         self.symbols[self.last_id] = symbol
         self.name_to_id[name] = self.last_id
 
@@ -133,10 +179,34 @@ class SymbolTable:
         return f"{self.symbols}"
 
 
+class Parenthesized(Expr):
+    def __init__(self, num_of_advances: int, inner: Expr) -> None:
+        super().__init__(num_of_advances)
+        self.inner = inner
+
+    def __repr__(self) -> str:
+        return f"Parenthesized({self.inner})"
+
+
+class ShuntingYardAlgorithmResults(Expr):
+    def __init__(
+        self, num_of_advances: int, operators: list[str], results: list[Expr]
+    ) -> None:
+        super().__init__(num_of_advances)
+        self.oeprators = operators
+        self.results = results
+
+    def __repr__(self) -> str:
+        return f"ShuntingYardAlgorithmResults({self.results}, ops={self.oeprators})"
+
+
 class Identifier(Expr):
-    def __init__(self, value: str) -> None:
-        super().__init__()
+    def __init__(
+        self, num_of_advances: int, value: str, for_assignment: bool = False
+    ) -> None:
+        super().__init__(num_of_advances)
         self.value = value
+        self.for_assignment = for_assignment
 
     def __repr__(self) -> str:
         return f"Ident({self.value})"
@@ -144,8 +214,8 @@ class Identifier(Expr):
 
 class TypeDef(Expr):
     # TODO: should name be Identifier?
-    def __init__(self, name: str) -> None:
-        super().__init__()
+    def __init__(self, num_of_advances: int, name: str) -> None:
+        super().__init__(num_of_advances)
         self.name = name
 
     def __repr__(self) -> str:
@@ -153,8 +223,10 @@ class TypeDef(Expr):
 
 
 class Lambda(Expr):
-    def __init__(self, parameters: SymbolTable, body: Expr) -> None:
-        super().__init__()
+    def __init__(
+        self, num_of_advances: int, parameters: SymbolTable, body: Expr
+    ) -> None:
+        super().__init__(num_of_advances)
         self.parameters = parameters
         self.body = body
 
@@ -163,8 +235,8 @@ class Lambda(Expr):
 
 
 class Assignment(Expr):
-    def __init__(self, left: Expr, right: Expr) -> None:
-        super().__init__()
+    def __init__(self, num_of_advances: int, left: Expr, right: Expr) -> None:
+        super().__init__(num_of_advances)
         self.left = left
         self.right = right
 
@@ -173,13 +245,16 @@ class Assignment(Expr):
 
 
 class Reference(Expr):
-    def __init__(self, name: str, belongs_to: int) -> None:
-        super().__init__()
+    def __init__(
+        self, num_of_advances: int, name: str, belongs_to: int, symbol_id: int
+    ) -> None:
+        super().__init__(num_of_advances)
         self.name = name
+        self.symbol_id = symbol_id
         self.belongs_to = belongs_to
 
     def __repr__(self) -> str:
-        return f"Ref(ST={self.belongs_to}, Ref={self.name})"
+        return f"Ref(ST={self.belongs_to}, Ref={self.name}, ID={self.symbol_id})"
 
 
 class Parameter(Expr):
@@ -193,12 +268,19 @@ class Parser(Cursor):
         self.results: list[Expr] = []
         zero_symbol_table = SymbolTable(0)
         # TODO: we are waiting for typedef!
-        zero_symbol_table.insert("int", Identifier("int"))
+        zero_symbol_table.insert("int", Identifier(0, "int"))
 
         self.symbol_tables: dict[int, SymbolTable] = {0: zero_symbol_table}
         self.using_st: int = 0
         self.parsing_lambda_parameters = False
-        self.op_stack: list[Expr] = []
+        self.op_stack: list[str] = []
+        # temp solution
+        self.current_number_of_advances = 0
+        self.already_parsing_sya = False
+
+    def advance(self) -> None:
+        self.current_number_of_advances += 1
+        return super().advance()
 
     def lookup(self, name: str, symbol_table_id: int | None = None) -> Symbol | None:
         symbol_table_id = self.using_st if symbol_table_id is None else symbol_table_id
@@ -218,33 +300,71 @@ class Parser(Cursor):
             if parsed is None:
                 break
             self.results.append(parsed)
-            self.advance()
+            self.current_number_of_advances = 0
 
     def parse(self) -> Expr | None:
         c = self.current()
+        result: Expr | None = None
         if c is None:
-            return None
+            result = None
         elif c.ty == TT.IDENT:
             if c.val is None:
                 raise Exception("Identifier with no value?")
             symbol = self.lookup(c.val)
-            if symbol is None:
+            if symbol is not None:
                 self.advance()
-                next = self.current()
+                result = Reference(
+                    self.current_number_of_advances, c.val, symbol.belongs_to, symbol.id
+                )
+            else:
+                next = self.input[self.at + 1]
                 if (
                     next is not None
                     and next.ty is TT.IDENT
                     and self.parsing_lambda_parameters
                     and self.using_st in self.symbol_tables
                 ):
+                    self.advance()
                     sym_table = self.symbol_tables[self.using_st]
                     expr = self.parse()
                     if expr is not None and isinstance(expr, Reference):
                         sym_table.insert(c.val, expr)
-                        return Parameter()
-                self.at -= 1
-                return Identifier(c.val)
-            return Reference(c.val, symbol.belongs_to)
+                        result = Parameter(self.current_number_of_advances)
+                else:
+                    self.advance()
+                    for_assignment = False
+                    if (
+                        (c2 := self.current())
+                        and c2 is not None
+                        and c2.ty == TT.BACKSLASH
+                    ):
+                        for_assignment = True
+                    result = Identifier(
+                        self.current_number_of_advances, c.val, for_assignment
+                    )
+
+        elif c.ty == TT.OPEN_PAREN:
+            self.advance()
+            the_between: list[Expr] = []
+            tuple: bool = False
+            while True:
+                c = self.current()
+                if c is not None:
+                    if c.ty == TT.CLOSE_PAREN:
+                        self.advance()
+                        break
+                expr = self.parse()
+                if expr is None:
+                    self.at -= 1
+                    break
+                the_between.append(expr)
+
+            if tuple:
+                raise Exception("TODO!1")
+            elif len(the_between) == 0:
+                raise Exception("TODO!2")
+            elif len(the_between) == 1:
+                result = Parenthesized(self.current_number_of_advances, the_between[0])
         elif c.ty == TT.BACKSLASH:
             self.advance()
             symbol_table_id = list(self.symbol_tables.items())[-1][0] + 1
@@ -267,7 +387,6 @@ class Parser(Cursor):
                             )
                         lambda_symbol_table.insert("ret", ret_type)
                         self.symbol_tables[symbol_table_id] = lambda_symbol_table
-                        self.advance()
                         c = self.current()
                         if c is None or c.ty is not TT.FUNCTION_ARROW:
                             raise Exception(
@@ -279,7 +398,6 @@ class Parser(Cursor):
                 if expr is None or not isinstance(expr, Parameter):
                     self.at -= 1
                     break
-                self.advance()
 
             body = self.parse()
             if body is None:
@@ -287,8 +405,49 @@ class Parser(Cursor):
             popped = self.results.pop()
             if not isinstance(popped, Identifier):
                 return popped
-            return Assignment(popped, Lambda(lambda_symbol_table, body))
-        raise Exception(f"Unhandled token: {c}")
+
+            result = Assignment(
+                self.current_number_of_advances,
+                popped,
+                Lambda(self.current_number_of_advances, lambda_symbol_table, body),
+            )
+        # At this point, past previous parsing, we should have advanced past
+        # the last token and now be face-to-face with the rare, elusive, OP!
+        c = self.current()
+        possible_op = get_op(c)
+        if not check_is_allowed(result) or result is None or self.already_parsing_sya:
+            return result
+        if possible_op is None:
+            return result
+
+        self.advance()
+        # NOW WE START! begin working with the shunting yard algorithm
+        # for parsing arithmetic
+        self.op_stack.append(possible_op[0])
+        self.already_parsing_sya = True
+        res: list[Expr] = [result]
+        while True:
+            c = self.current()
+            possible_op = get_op(c)
+            if c is None:
+                break
+            elif possible_op is not None:
+                self.op_stack.append(possible_op[0])
+                self.advance()
+                continue
+
+            parsed = self.parse()
+            if not check_is_allowed(parsed) or parsed is None:
+                self.at -= 1
+                break
+            res.append(parsed)
+
+        sya_res = ShuntingYardAlgorithmResults(
+            self.current_number_of_advances, self.op_stack, res
+        )
+        self.op_stack = []
+        self.already_parsing_sya = False
+        return sya_res
 
 
 def run():
